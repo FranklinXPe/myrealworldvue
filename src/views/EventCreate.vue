ad7f63aaa3ec514dbbef5ff0a7fbf6b8fac6cca4
FranklinXpe <dexpega@gmail.com>
2020-01-02 16:54:39 -0500

    Configuracion de Vue-router

---
+++ src/views/EventCreate.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1>
+    Create Event
+  </h1>
+</template>
---
+++ src/views/EventList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Event Listing</h1>
+    <EventCard />
+    <BaseIcon></BaseIcon>
+  </div>
+</template>
+
+<script>
+import EventCard from '@/components/EventCard.vue'
+export default {
+  name: 'event-card',
+  components: { EventCard }
+}
+</script>
